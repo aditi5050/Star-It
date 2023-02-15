9eb6e84a661364add312b37cb11f1da20fe81431
Aditi Goyal <102579070+aditi5050@users.noreply.github.com>
2023-02-15 20:37:48 +0530

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://staritservices.net/assets/images/logo/Star-IT-Services-logo.jpg" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
